fbf2de1c0ead032582a2b53ec771c6d229d7a7ab
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-02-12 15:38:18 -0500

    Test: Added more redux tests

--- src/store/actions/actions.test.js
+++ src/store/actions/actions.test.js
@@ -1,15 +1,21 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { updateSpeciesSelected, updateLocationSelected } from './actions';
+import {
+  updateSpeciesSelected,
+  updateLocationSelected,
+  updateUserCoordinates,
+  updateError,
+  updatePageNumber,
+} from './actions';
 import {
   mockSpeciesSelectedData,
   mockLocationSelectedData,
-} from '../../utility/testing/MockData';
+} from '../../utility/testing/mockData';
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
 describe('actions tests', () => {
-  it('updateSpeciesSelected should dispatch a "update species selection" action and update speciesSelected with mock data', () => {
+  it('updateSpeciesSelected should dispatch an "UPDATE_SPECIES_SELECTED" action and update speciesSelected with mock data', () => {
     const initialState = {};
     const store = mockStore(initialState);
 
@@ -23,16 +29,58 @@ describe('actions tests', () => {
     expect(actions).toEqual([expectedPayload]);
   });
 
-  it('updateLocationSelected should dispatch a update location selection action and update locationSelected with mock data', () => {
+  it('updateLocationSelected should dispatch an "UPDATE_LOCATION_SELECTED" action and update locationSelected with mock data', () => {
     const initialState = {};
     const store = mockStore(initialState);
 
-    store.dispatch(updateLocationSelected(mocklocationSelectedData));
+    store.dispatch(updateLocationSelected(mockLocationSelectedData));
 
     const actions = store.getActions();
     const expectedPayload = {
       type: 'UPDATE_LOCATION_SELECTED',
       locationSelected: mockLocationSelectedData,
+    };
+    expect(actions).toEqual([expectedPayload]);
+  });
+
+  it('updateUserCoordinates should dispatch an "UPDATE_USER_COORDINATES" action and update userCoordinates with mock data', () => {
+    const initialState = {};
+    const store = mockStore(initialState);
+
+    store.dispatch(updateUserCoordinates([43.653226, -79.3831843]));
+
+    const actions = store.getActions();
+    const expectedPayload = {
+      type: 'UPDATE_USER_COORDINATES',
+      userCoordinates: [43.653226, -79.3831843],
+    };
+    expect(actions).toEqual([expectedPayload]);
+  });
+
+  it('updateError should dispatch an "UPDATE_ERROR" action and update error with mock data', () => {
+    const initialState = {};
+    const store = mockStore(initialState);
+
+    store.dispatch(updateError('500 Internal Server Error'));
+
+    const actions = store.getActions();
+    const expectedPayload = {
+      type: 'UPDATE_ERROR',
+      error: '500 Internal Server Error',
+    };
+    expect(actions).toEqual([expectedPayload]);
+  });
+
+  it('updatePageNumber should dispatch an "UPDATE_PAGE_NUMBER" action and update pageNumber with mock data', () => {
+    const initialState = {};
+    const store = mockStore(initialState);
+
+    store.dispatch(updatePageNumber(2));
+
+    const actions = store.getActions();
+    const expectedPayload = {
+      type: 'UPDATE_PAGE_NUMBER',
+      pageNumber: 2,
     };
     expect(actions).toEqual([expectedPayload]);
   });
---
+++ src/store/reducer.test.js
@@ -0,0 +1,85 @@
+import reducer, { initialState } from './reducer';
+import * as actionTypes from './actions/actionTypes';
+import {
+  mockSpeciesSelectedData,
+  mockLocationSelectedData,
+} from '../utility/testing/mockData';
+
+describe('reducer tests', () => {
+  it('UPDATE_SPECIES_SELECTED should update speciesSelected and set pageNumber to null', () => {
+    const newSpeciesSelected = mockSpeciesSelectedData;
+    expect(
+      reducer(initialState, {
+        type: actionTypes.UPDATE_SPECIES_SELECTED,
+        speciesSelected: newSpeciesSelected,
+      })
+    ).toEqual({
+      locationSelected: null,
+      speciesSelected: mockSpeciesSelectedData,
+      userCoordinates: null,
+      error: null,
+      pageNumber: null,
+    });
+  });
+
+  it('UPDATE_LOCATION_SELECTED should update locationSelected and set pageNumber/userCoordinates to null', () => {
+    const newLocationSelected = mockLocationSelectedData;
+    expect(
+      reducer(initialState, {
+        type: actionTypes.UPDATE_LOCATION_SELECTED,
+        locationSelected: newLocationSelected,
+      })
+    ).toEqual({
+      locationSelected: mockLocationSelectedData,
+      speciesSelected: null,
+      userCoordinates: null,
+      error: null,
+      pageNumber: null,
+    });
+  });
+
+  it('UPDATE_USER_COORDINATES should update userCoordinates and set pageNumber/locationSelected to null', () => {
+    expect(
+      reducer(initialState, {
+        type: actionTypes.UPDATE_USER_COORDINATES,
+        userCoordinates: [43.653226, -79.3831843],
+      })
+    ).toEqual({
+      locationSelected: null,
+      speciesSelected: null,
+      userCoordinates: [43.653226, -79.3831843],
+      error: null,
+      pageNumber: null,
+    });
+  });
+
+  it('UPDATE_ERROR should update error and set pageNumber to null', () => {
+    expect(
+      reducer(initialState, {
+        type: actionTypes.UPDATE_ERROR,
+        error: '500 Internal Server Error',
+      })
+    ).toEqual({
+      locationSelected: null,
+      speciesSelected: null,
+      userCoordinates: null,
+      error: '500 Internal Server Error',
+      pageNumber: null,
+    });
+  });
+
+  it('UPDATE_PAGE_NUMBER should update pageNumber', () => {
+    expect(
+      reducer(initialState, {
+        type: actionTypes.UPDATE_PAGE_NUMBER,
+        pageNumber: 3,
+      })
+    ).toEqual({
+      locationSelected: null,
+      speciesSelected: null,
+      userCoordinates: null,
+      error: null,
+      pageNumber: 3,
+    });
+  });
+});
