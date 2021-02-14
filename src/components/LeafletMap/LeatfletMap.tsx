55fd8b76294ff219c29cf6b34daf01eb6f1f609d
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-02-14 12:52:37 -0500

    Add: LeafletMap component. Refactor: MapDisplay

---
+++ src/components/LeafletMap/LeatfletMap.tsx
@@ -0,0 +1,52 @@
+import React, { Fragment } from 'react';
+
+// React Leaflet
+import { MapContainer, TileLayer } from 'react-leaflet';
+
+// MUI
+import Typography from '@material-ui/core/Typography';
+
+// Types
+import { SpeciesSelected } from '../../store/reduxTypes';
+import { LatLngExpression } from 'leaflet';
+
+interface Props {
+  speciesSelected?: SpeciesSelected;
+  key: string | number | null | undefined;
+  center: LatLngExpression | undefined;
+  className: string;
+  zoom: number;
+}
+
+function LeatfletMap(props: Props) {
+  return (
+    <Fragment>
+      {props.speciesSelected && (
+        <Typography variant="h6" component="h6" style={{ textAlign: 'center' }}>
+          Select a location and a species to view a heatmap of recent
+          observations
+        </Typography>
+      )}
+      <MapContainer
+        key={props.key}
+        className={props.className}
+        center={props.center}
+        zoom={props.zoom}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {props.speciesSelected && (
+          <TileLayer
+            attribution='<a href="https://www.inaturalist.org/">iNaturalist</a>'
+            url={`https://api.inaturalist.org/v1/colored_heatmap/{z}/{x}/{y}.png?color=blue&quality_grade=research&taxon_id=${props.speciesSelected.id}`}
+          />
+        )}
+      </MapContainer>
+    </Fragment>
+  );
+}
+
+export default LeatfletMap;
--- src/containers/MapDisplay/MapDisplay.tsx
+++ src/containers/MapDisplay/MapDisplay.tsx
@@ -1,5 +1,4 @@
-import React, { Fragment } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { Fragment, ReactNode } from 'react';
 import './MapDisplay.css';
 
 // Utility Functions
@@ -8,8 +7,8 @@ import { googleAnalytics } from '../../utility/utilityFunctions';
 // Redux
 import { useSelector } from 'react-redux';
 
-// MUI
-import Typography from '@material-ui/core/Typography';
+// Components
+import LeatfletMap from '../../components/LeafletMap/LeatfletMap';
 
 interface StateProps {
   locationSelected: {
@@ -57,50 +56,29 @@ function MapDisplay() {
     (state: StateProps) => state.userCoordinates
   );
 
-  let leafletDisplay = null;
-  let mapKey = null;
+  let leafletDisplay: ReactNode | null = null;
+  let mapKey: string | number[] | null = null;
   let coordinates = [40.7812, -73.9665];
 
   if (!locationSelected && !speciesSelected && !userCoordinates) {
     leafletDisplay = (
-      <div style={{ textAlign: 'center' }}>
-        <Typography variant="h6" component="h6">
-          Select a location and a species to view a heatmap of recent
-          observations
-        </Typography>
-        <MapContainer
-          key={coordinates[0] + coordinates[1]}
-          className="MapNoInfo"
-          center={[coordinates[0], coordinates[1]]}
-          zoom={14}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </MapContainer>
-      </div>
+      <LeatfletMap
+        key={coordinates[0] + coordinates[1]}
+        className="MapNoInfo"
+        center={[coordinates[0], coordinates[1]]}
+        zoom={14}
+      />
     );
   } else if (speciesSelected && !locationSelected && !userCoordinates) {
     mapKey = coordinates.join('') + speciesSelected.id;
     leafletDisplay = (
-      <MapContainer
+      <LeatfletMap
         key={mapKey}
         className="MapID"
         center={[coordinates[0], coordinates[1]]}
         zoom={2}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <TileLayer
-          attribution='<a href="https://www.inaturalist.org/">iNaturalist</a>'
-          url={`https://api.inaturalist.org/v1/colored_heatmap/{z}/{x}/{y}.png?color=blue&quality_grade=research&taxon_id=${speciesSelected.id}`}
-        />
-      </MapContainer>
+        speciesSelected={speciesSelected}
+      />
     );
   } else if (speciesSelected && locationSelected?.length > 0) {
     coordinates = [
@@ -109,22 +87,13 @@ function MapDisplay() {
     ];
     mapKey = coordinates.join('') + speciesSelected.id;
     leafletDisplay = (
-      <MapContainer
+      <LeatfletMap
         key={mapKey}
         className="MapID"
         center={[coordinates[0], coordinates[1]]}
         zoom={14}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <TileLayer
-          attribution='<a href="https://www.inaturalist.org/">iNaturalist</a>'
-          url={`https://api.inaturalist.org/v1/colored_heatmap/{z}/{x}/{y}.png?color=blue&quality_grade=research&taxon_id=${speciesSelected.id}`}
-        />
-      </MapContainer>
+        speciesSelected={speciesSelected}
+      />
     );
   } else if (!speciesSelected && locationSelected?.length > 0) {
     mapKey = [
@@ -132,70 +101,37 @@ function MapDisplay() {
       locationSelected[0]?.geometry.location.lng,
     ];
     leafletDisplay = (
-      <div style={{ textAlign: 'center' }}>
-        <Typography variant="h6" component="h6">
-          Select a location and a species to view a heatmap of recent
-          observations
-        </Typography>
-        <MapContainer
-          key={mapKey.join('')}
-          className="MapID"
-          center={[
-            locationSelected[0]?.geometry.location.lat,
-            locationSelected[0]?.geometry.location.lng,
-          ]}
-          zoom={14}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </MapContainer>
-      </div>
+      <LeatfletMap
+        key={mapKey.join('')}
+        className="MapID"
+        center={[
+          locationSelected[0]?.geometry.location.lat,
+          locationSelected[0]?.geometry.location.lng,
+        ]}
+        zoom={14}
+      />
     );
   } else if (speciesSelected && userCoordinates) {
     coordinates = [userCoordinates[0], userCoordinates[1]];
     mapKey = coordinates.join('') + speciesSelected.id;
     leafletDisplay = (
-      <MapContainer
+      <LeatfletMap
         key={mapKey}
         className="MapID"
         center={[coordinates[0], coordinates[1]]}
         zoom={14}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <TileLayer
-          attribution='<a href="https://www.inaturalist.org/">iNaturalist</a>'
-          url={`https://api.inaturalist.org/v1/colored_heatmap/{z}/{x}/{y}.png?color=blue&quality_grade=research&taxon_id=${speciesSelected.id}`}
-        />
-      </MapContainer>
+        speciesSelected={speciesSelected}
+      />
     );
   } else if (!speciesSelected && userCoordinates) {
     mapKey = [userCoordinates[0], userCoordinates[1]];
     leafletDisplay = (
-      <div style={{ textAlign: 'center' }}>
-        <Typography variant="h6" component="h6">
-          Select a location and a species to view a heatmap of recent
-          observations
-        </Typography>
-        <MapContainer
-          key={mapKey.join('')}
-          className="MapID"
-          center={[userCoordinates[0], userCoordinates[1]]}
-          zoom={14}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </MapContainer>
-      </div>
+      <LeatfletMap
+        key={mapKey.join('')}
+        className="MapID"
+        center={[userCoordinates[0], userCoordinates[1]]}
+        zoom={14}
+      />
     );
   }
 
