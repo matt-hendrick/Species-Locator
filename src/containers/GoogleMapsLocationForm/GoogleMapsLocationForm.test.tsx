acbc7d2d101689c7b8190cccec467c792b3690c6
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-02-18 12:16:03 -0500

    Test: Improved form tests

--- src/containers/GoogleMapsLocationForm/GoogleMapsLocationForm.test.tsx
+++ src/containers/GoogleMapsLocationForm/GoogleMapsLocationForm.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render, screen } from '../../utility/testing/reduxTestUtils';
 import GoogleMapsLocationForm from './GoogleMapsLocationForm';
+import userEvent from '@testing-library/user-event';
 
 describe('GoogleMapsLocationForm tests', () => {
   it('Renders GoogleMapsLocationForm with label text "Location Selector"', () => {
     render(<GoogleMapsLocationForm />);
 
     expect(screen.getByLabelText(/Location Selector/i)).toBeInTheDocument();
+
+    userEvent.type(screen.getByLabelText(/Location Selector/i), 'Toronto');
+    expect(screen.getByLabelText(/Location Selector/i)).toHaveValue('Toronto');
+
+    expect(screen.getByText(/Search a location/i)).toBeInTheDocument();
   });
 });
--- src/containers/SpeciesForm/SpeciesForm.test.tsx
+++ src/containers/SpeciesForm/SpeciesForm.test.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { render, screen } from '../../utility/testing/reduxTestUtils';
 import SpeciesForm from './SpeciesForm';
+import userEvent from '@testing-library/user-event';
 
 describe('SpeciesForm tests', () => {
-  it('On init, renders SpeciesForm with label text "Species Selector"', () => {
+  it(`Renders SpeciesForm with entered value when valid text entered. 
+  Also displays dropdown "search a species" text after input`, () => {
     render(<SpeciesForm />);
 
     expect(screen.getByLabelText(/Species Selector/i)).toBeInTheDocument();
+
+    userEvent.type(screen.getByLabelText(/Species Selector/i), 'Bear');
+    expect(screen.getByLabelText(/Species Selector/i)).toHaveValue('Bear');
+
+    expect(screen.getByText(/Search a species/i)).toBeInTheDocument();
   });
 });
